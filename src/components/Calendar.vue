7f04ed51aa87d6d6fb986f6aebeff21ed4a7ef64
Javi Sastre <javisastrecal@gmail.com>
2022-04-10 13:17:01 +0200

    modal work in progress

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,87 +1,98 @@
 <template>
-  <div class="container mx-auto h-screen pt-5">
-    <div class="flex justify-end px-10">
-      <button
-        @click="setToday"
-        class="text-neutral-600 border-2 flex gap-3 items-center hover:bg-neutral-600 hover:text-neutral-200 duration-200 border-neutral-600 px-4 py-1 rounded text-xs"
-      >
-        <div class="font-signika font-bold">Today</div>
-        <i class="fa-solid fa-calendar text-lg"></i>
-      </button>
+  <div class="relative">
+    <div v-if="date">
+      <Modal class="absolute z-10" :date="date" @killModal="deleteModal" />
     </div>
-    <div
-      class="flex justify-between px-10 items-end border mt-4 border-b-0 border-neutral-500 py-4 text-neutral-600"
-    >
-      <button
-        @click="prevMonth"
-        class="hover:scale-110 duration-100 hover:-translate-x-2"
-      >
-        <i class="text-2xl fa-solid fa-arrow-left"></i>
-      </button>
-      <h2 class="w-1/2 text-center font-bold text-3xl font-swash text-rose-500">
-        {{ monthList[month] }} {{ year }}
-      </h2>
-      <button
-        @click="nextMonth"
-        class="hover:scale-110 duration-100 hover:translate-x-2"
-      >
-        <i class="text-2xl fa-solid fa-arrow-right"></i>
-      </button>
-    </div>
-    <div
-      class="text-neutral-600 grid grid-cols-7 px-10 border border-b-0 border-neutral-500 py-1"
-    >
-      <div
-        class="self-center font-signika text-neutral-600 text-center"
-        v-for="weekDay in weekDays"
-        :key="weekDay"
-      >
-        {{ weekDay }}
-      </div>
-    </div>
-    <main class="grid grid-cols-7 gap-1 border py-4 px-10 border-neutral-500">
-      <div
-        v-for="day in prevMonthDays"
-        :key="day"
-        @click="prevMonth"
-        class="p-2 text-right h-28 flex flex-col items-end hover:cursor-pointer bg-neutral-200 border border-neutral-400 text-neutral-500"
-      >
-        <div class="text-center text-sm">
-          {{ day.getDate() }}
-        </div>
+    <div class="container mx-auto h-screen pt-5">
+      <div class="flex justify-end px-10">
+        <button
+          @click="setToday"
+          class="text-neutral-600 border-2 flex gap-3 items-center hover:bg-neutral-600 hover:text-neutral-200 duration-200 border-neutral-600 px-4 py-1 rounded text-xs"
+        >
+          <div class="font-signika font-bold">Today</div>
+          <i class="fa-solid fa-calendar text-lg"></i>
+        </button>
       </div>
       <div
-        v-for="day in days"
-        :key="day"
-        :class="{ [todayClassBox]: isToday(day) }"
-        class="p-2 text-right h-28 flex flex-col items-end border border-neutral-400 bg-neutral-50 text-neutral-700"
+        class="flex justify-between px-10 items-end border mt-4 border-b-0 border-neutral-500 py-4 text-neutral-600"
+      >
+        <button
+          @click="prevMonth"
+          class="hover:scale-110 duration-100 hover:-translate-x-2"
+        >
+          <i class="text-2xl fa-solid fa-arrow-left"></i>
+        </button>
+        <h2
+          class="w-1/2 text-center font-bold text-3xl font-swash text-rose-500"
+        >
+          {{ monthList[month] }} {{ year }}
+        </h2>
+        <button
+          @click="nextMonth"
+          class="hover:scale-110 duration-100 hover:translate-x-2"
+        >
+          <i class="text-2xl fa-solid fa-arrow-right"></i>
+        </button>
+      </div>
+      <div
+        class="text-neutral-600 grid grid-cols-7 px-10 border border-b-0 border-neutral-500 py-1"
       >
         <div
-          class="text-center text-sm"
-          :class="{ [todayClassNumber]: isToday(day) }"
+          class="self-center font-signika text-neutral-600 text-center"
+          v-for="weekDay in weekDays"
+          :key="weekDay"
         >
-          {{ day.getDate() }}
+          {{ weekDay }}
         </div>
       </div>
-      <div
-        v-for="day in nextMonthDays"
-        :key="day"
-        class="p-2 text-right h-28 flex flex-col items-end bg-neutral-200 border hover:cursor-pointer border-neutral-400 text-neutral-500"
-        @click="nextMonth"
-      >
-        <div class="text-center text-sm">
-          {{ day.getDate() }}
+      <main class="grid grid-cols-7 gap-1 border py-4 px-10 border-neutral-500">
+        <div
+          v-for="day in prevMonthDays"
+          :key="day"
+          @click="prevMonth"
+          class="p-2 text-right h-20 flex flex-col items-end hover:cursor-pointer bg-neutral-200 border border-neutral-400 text-neutral-500"
+        >
+          <div class="text-center text-sm">
+            {{ day.getDate() }}
+          </div>
         </div>
-      </div>
-    </main>
+        <div
+          v-for="day in days"
+          :key="day"
+          :class="{ [todayClassBox]: isToday(day) }"
+          class="p-2 text-right h-20 flex flex-col items-end border border-neutral-400 bg-neutral-50 text-neutral-700"
+          @click="date = day"
+        >
+          <div
+            class="text-center text-sm"
+            :class="{ [todayClassNumber]: isToday(day) }"
+          >
+            {{ day.getDate() }}
+          </div>
+        </div>
+        <div
+          v-for="day in nextMonthDays"
+          :key="day"
+          class="p-2 text-right h-20 flex flex-col items-end bg-neutral-200 border hover:cursor-pointer border-neutral-400 text-neutral-500"
+          @click="nextMonth"
+        >
+          <div class="text-center text-sm">
+            {{ day.getDate() }}
+          </div>
+        </div>
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
+import Modal from "./Modal.vue";
 export default {
   name: "Calendar",
+  components: { Modal },
   data() {
     return {
+      date: null,
       month: new Date().getMonth(),
       year: new Date().getFullYear(),
       monthList: [
@@ -139,6 +150,9 @@ export default {
     setToday() {
       this.month = new Date().getMonth();
       this.year = new Date().getFullYear();
+    },
+    deleteModal() {
+      this.date = null;
     },
   },
   computed: {
---
+++ src/components/Modal.vue
@@ -0,0 +1,74 @@
+<template>
+  <div
+    class="w-screen h-screen bg-neutral-700/20 flex justify-center items-center"
+    @click="killModal"
+  >
+    <div class="bg-neutral-50 text-center w-1/4 rounded-lg shadow-2xl p-2">
+      <div @click="killModal" class="flex justify-end">
+        <i class="fa-solid fa-xmark hover:cursor-pointer"></i>
+      </div>
+      <div class="flex flex-col gap-4 p-10">
+        <h2 class="font-swash text-xl">Add a new event</h2>
+        <p class="font-bold text-rose-600 font-signika">{{ formattedDate }}</p>
+        <input
+          placeholder="title"
+          :class="inputClass"
+          type="text"
+          v-model="title"
+        />
+        <input
+          placeholder="description"
+          type="text"
+          :class="inputClass"
+          v-model="description"
+        />
+        <button :class="buttonClass" :disabled="!description || !title">
+          Add
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  props: { date: { type: Date } },
+  data() {
+    return {
+      title: "",
+      description: "",
+      inputClass:
+        "border border-neutral-400 rounded  hover:border-neutral-600 px-4 py-1 placeholder-neutral-400 focus:outline-none focus:border-neutral-600  focus:placeholder-neutral-600",
+      buttonClass:
+        "border-2 border-neutral-600 self-center px-4 py-1 hover:bg-neutral-600 hover:text-neutral-100 rounded-md hover:cursor-pointer shadow-md disabled:shadow-none disabled:cursor-not-allowed disabled:bg-neutral-100 disabled:border-neutral-300 disabled:text-neutral-400",
+      monthList: [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ],
+    };
+  },
+  computed: {
+    formattedDate() {
+      return `${this.date.getDate()} of ${
+        this.monthList[this.date.getMonth()]
+      } of ${this.date.getFullYear()}`;
+    },
+  },
+  methods: {
+    killModal() {
+      this.$emit("killModal");
+    },
+  },
+};
+</script>
